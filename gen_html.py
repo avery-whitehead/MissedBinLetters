65ee8f9673cd06bd7c7253b5e769015a82ded5ee
james-whitehead <jameswhthead@gmail.com>
2018-06-08 17:20:32 +0100

    Changed folder structure to separate folders for garden waste and recycling, created HTML templates for separate letters, split apart save_and_convert_html into two different functions

--- gen_html.py
+++ gen_html.py
@@ -6,6 +6,7 @@ From the command line, run `py -3 gen_html.py`
 How it works:
 Generates an HTML file and uses wkhtmltopdf to convert it to a PDF
 """
+import glob
 import subprocess
 import datetime
 import sys
@@ -34,11 +35,14 @@ class GardenWasteRequest(Request):
     """
     def __init__(self, occup: str, addr: str, case_ref: str, num_subs: str):
         """
+        Initialises the different attributes between GardenWasteRequest and
+        Request
         Args:
             num_subs (str): The number of subscriptions the property has
         """
         super().__init__(occup, addr, case_ref)
         self.num_subs = num_subs
+        self.req_type = 'gw'
 
 
 class RecyclingRequest(Request):
@@ -47,9 +51,11 @@ class RecyclingRequest(Request):
     """
     def __init__(self, occup: str, addr: str, case_ref: str):
         """
-        No different to the parent Request class, so has no extra attributes
+        Initialises the different attributes between RecyclingRequest and
+        Request
         """
         super().__init__(occup, addr, case_ref)
+        self.req_type = 'rec'
 
 def query_gw_requests() -> list:
     """
@@ -107,58 +113,179 @@ def create_html(request: Request) -> str:
     Returns:
         (str): The HTLM template with the information filed in
     """
+    if isinstance(request, GardenWasteRequest):
+        content = '' \
+        '<h1>\n' \
+        'Garden Waste Letter\n' \
+        '</h1>\n' \
+        '<p>\n' \
+        'Further to your report of a missed garden waste bin collection. As ' \
+        'you are most probably aware as of 4 June refuse, recycling and ' \
+        'garden waste collections have been revised to improve the ' \
+        'efficiency of the service.  These changes involve new collection ' \
+        'routes with different drivers and have resulted in occasional ' \
+        'missed collections.\n' \
+        '</p>\n' \
+        '<p>\n' \
+        'Unfortunately we are unable to return to collect your bin(s), ' \
+        'however please find enclosed 3 sacks for your garden waste. ' \
+        'Please affix 1 of the enclosed stickers to each sack so that the ' \
+        'licence can be seen by the collection crews.  Please ensure sacks ' \
+        'are tied intact and not overflowing.\n' \
+        '</p>\n' \
+        '<p>\n' \
+        'Please present filled sacks alongside your green wheeled bin by ' \
+        '06:00 on your collection day. Please ensure that your green waste ' \
+        'bin(s) correctly display current garden waste licences using the ' \
+        'ties provided.\n' \
+        '</p>\n' \
+        '<p>\n' \
+        'Thank you for your patience during this time.\n' \
+        '</p>\n' \
+        '<p>\n' \
+        '<strong>Hambleton District Council</strong>\n' \
+        '<br>\n' \
+        '01609 779977\n' \
+        '<br>\n' \
+        'info@hambleton.gov.uk\n' \
+        '</p>\n'
+    elif isinstance(request, RecyclingRequest):
+        content = '' \
+        '<h1>\n' \
+        'Recycling letter\n' \
+        '</h1>\n' \
+        '<p>\n' \
+        'Further to your report of a missed garden waste bin collection. As ' \
+        'you are most probably aware as of 4 June refuse, recycling and ' \
+        'garden waste collections have been revised to improve the ' \
+        'efficiency of the service.  These changes involve new collection ' \
+        'routes with different drivers and have resulted in occasional ' \
+        'missed collections.\n' \
+        '</p>\n' \
+        '<p>\n' \
+        'Please ensure sacks are tied, intact and not overflowing.\n' \
+        '</p>\n' \
+        '<p>\n' \
+        'Please present filled sacks alongside your recycling bin by 06:00 ' \
+        'on your collection day.\n' \
+        '</p>\n' \
+        '<p>\n' \
+        'Please note this is the only type of plastic bag which can be used ' \
+        'to contain your recycling, all other forms of plastic bags such as ' \
+        'shopping bags or black plastic bin bags are not recyclable on our ' \
+        'kerbside collection scheme and must be kept out of your recycling ' \
+        'bin.\n' \
+        '</p>\n' \
+        '<p>\n' \
+        'Please refer to your collection day postcard or visit ' \
+        'hambleton.go.uk to check collect day details and for other ' \
+        'information.\n' \
+        '</p>\n' \
+        '<p>\n' \
+        'Thank you for your patience during this time.\n' \
+        '</p>\n' \
+        '<p>\n' \
+        '<strong>Hambleton District Council</strong>\n' \
+        '<br>\n' \
+        '01609 779977\n' \
+        '<br>\n' \
+        'info@hambleton.gov.uk\n' \
+        '</p>\n'
     html = '' \
         '<!DOCTYPE html>\n' \
         '<html>\n' \
         '<head>\n' \
-        '    <style>\n' \
-        '        body {\n' \
-        '            font-family: sans-serif;\n' \
-        '        }\n' \
-        '        .addr {\n' \
-        '            font-weight: bold;\n' \
-        '            padding-left: 100px;\n' \
-        '            padding-top: 175px;\n' \
-        '            font-size: 22px;\n' \
-        '        }\n' \
-        '    </style>\n' \
+        '<style>\n' \
+        'body {\n' \
+        'font-family: sans-serif;\n' \
+        '}\n' \
+        'h1 {\n' \
+        'font-weight: normal;\n' \
+        'text-align: center;\n' \
+        'font-size: 36pt;\n' \
+        'text-decoration: underline;\n' \
+        '}\n' \
+        ' p {\n' \
+        'font-size: 11pt;\n' \
+        '}' \
+        '.addr {\n' \
+        'font-weight: bold;\n' \
+        'padding-left: 100px;\n' \
+        ' padding-top: 175px;\n' \
+        'font-size: 22px;\n' \
+        'page-break-after: always;\n' \
+        '}\n' \
+        '.content {\n' \
+        'font-family: "Calibri"\n' \
+        '}\n' \
+        '</style>\n' \
         '</head>\n' \
         '<body>\n' \
-        '    <section>\n' \
-        '        <div class="addr">\n' \
-       f'            {request.addr}\n' \
-        '        </div>\n' \
-        '    </section>\n' \
+        '<section>\n' \
+        '<div class="addr">\n' \
+       f'{request.addr}\n' \
+        '</div>\n' \
+        '<div class="content">\n' \
+       f'{content}\n' \
+        '</div>\n' \
+        '</section>\n' \
         '</body>\n' \
         '</html>'
+
     return html
 
-def save_and_convert_html(html: str, request: Request) -> str:
+def save_html(html: str, request: Request) -> None:
     """
-    Writes the HTML to file, calls the wkhtmltopdf process and converts
-    that file to a PDF
+    Writes the HTML to file, creating additional files for properties with
+    more than one garden waste subscription
     Args:
         html (str): The HTML to write to file and later convert
         request (Request): The Request this HTML was generated from
-    Returns:
-        (str): A string indicating success
     """
     try:
-        wkhtml_path = '"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"'
-        html_path = f'.\\htmls\\{request.case_ref}.html'
-        pdf_path = f'.\\pdfs\\{request.case_ref}.pdf'
+        dir_path = f'.\\htmls\\{request.req_type}'
+        html_path = f'{dir_path}\\{request.case_ref}-1.html'
         with open(html_path, 'w+') as html_f:
             html_f.write(html)
-        args = f'{wkhtml_path} {html_path} {pdf_path}'
-        subprocess.call(args, shell=False)
-        success_str = f'{SYSTIME} - Successfully created {pdf_path}'
+        success_str = f'{SYSTIME} - Successfully saved {html_path}'
+        print(success_str)
         with open('.\\missed_bin_letters.log', 'a') as log:
             log.write(f'{success_str}\n')
-        return success_str
+        # If there is more than one license, create multiple letters
+        if isinstance(request, GardenWasteRequest):
+            for i in range(1, int(request.num_subs)):
+                html_path = f'{dir_path}\\{request.case_ref}-{i + 1}.html'
+                with open(html_path, 'w+') as html_f:
+                    html_f.write(html)
+            success_str = f'{SYSTIME} - Successfully saved {html_path}'
+            print(success_str)
+            with open('.\\missed_bin_letters.log', 'a') as log:
+                log.write(f'{success_str}\n')
     except (IOError, FileNotFoundError) as error:
-        with open('.\\missed_bin_letters.log', 'a') as log:
-            log.write(f'{SYSTIME} - {error}\n')
-        sys.exit(1)
+        log_error('.\\missed_bin_letters.log', error)
+
+def convert_html() -> None:
+    """
+    Converts each HTML file to a PDF using wkhtmltopdf
+    """
+    try:
+        exe = '"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"'
+        html_dirs = ['.\\htmls\\gw\\*', '.\\htmls\\rec\\*']
+        for html_dir in html_dirs:
+            htmls = glob.glob(html_dir)
+            for html in htmls:
+                if html_dir == '.\\htmls\\gw\\*':
+                    case_ref = html[11:-5]
+                    pdf = f'.\\pdfs\\gw\\{case_ref}.pdf'
+                elif html_dir == '.\\htmls\\rec\\*':
+                    case_ref = html[12:-5]
+                    pdf = f'.\\pdfs\\rec\\{case_ref}.pdf'
+                flags = '-B 25.4mm -L 25.4mm -R 25.4mm -T 25.4mm'
+                args = f'{exe} --disable-smart-shrinking {flags} {html} {pdf}'
+                print(args)
+                subprocess.call(args, shell=False)
+    except (IOError, FileNotFoundError) as error:
+        log_error('.\\missed_bin_letters.log', error)
 
 def update_database(request: Request) -> str:
     """
@@ -183,9 +310,19 @@ def update_database(request: Request) -> str:
             log.write(f'{success_str}\n')
         return success_str
     except (pyodbc.DatabaseError, pyodbc.InterfaceError) as error:
-        with open('.\\missed_bin_letters', 'a') as log:
-            log.write(f'{SYSTIME} - {error}\n')
-        sys.exit(1)
+        log_error('.\\missed_bin_letters.log', error)
+
+
+def log_error(log_path: str, error: Exception):
+    """
+    Writes exception messages to the log file and exits the program
+    Args:
+        log_path (str): The path of the log file to write to
+        error (Exception): The error message given by an exception
+    """
+    with open(log_path, 'a') as log:
+        log.write(f'{SYSTIME} - {error}\n')
+    sys.exit(1)
 
 
 if __name__ == '__main__':
@@ -200,12 +337,10 @@ if __name__ == '__main__':
             uid=config['uid'],
             pwd=config['pwd'])
     except (pyodbc.DatabaseError, pyodbc.InterfaceError) as error:
-        with open('.\\missed_bin_letters', 'a') as log:
-            log.write(f'{SYSTIME} - {error}\n')
-        sys.exit(1)
+        log_error('.\\missed_bin_letters.log', error)
     gw_requests = query_gw_requests()
     rec_requests = query_rec_requests()
     for request in gw_requests + rec_requests:
         html = create_html(request)
-        print(save_and_convert_html(html, request))
-        print(update_database(request))
+        save_html(html, request)
+    convert_html()
--- gw_update.sql
+++ gw_update.sql
@@ -2,6 +2,6 @@
 DECLARE @gw_cutoff datetime = '2018-06-07 12:00:00.00';
 UPDATE HDC_AF_GW_Missed_Collections.dbo.Missed_Collections
 SET GWSacksLetterSent = 1
-WHERE HDC_AF_GW_Missed_Collections.dbo.Missed_Collections.id.id NOT IN (12, 13, 17)
+WHERE HDC_AF_GW_Missed_Collections.dbo.Missed_Collections.id NOT IN (12, 13, 17)
 AND GWSacksRequested = 'yes' AND GWSacksLetterSent = 0
 and AddedDateTime < @gw_cutoff
